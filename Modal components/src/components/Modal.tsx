524f51eada00a77a14d4d1c32451bf57b409b67b
barrystone <barrystone3183@gmail.com>
2023-03-07 09:43:56 +0800

    style: Modal Component

--- Modal components/src/components/Modal.tsx
+++ Modal components/src/components/Modal.tsx
@@ -6,8 +6,42 @@ interface ModalProps {
 }
 const ModalBackground = styled.div`
   position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
 `;
-const ModalBody = styled.div``;
+const ModalBody = styled.div`
+  background-color: white;
+  color: black;
+  margin: 10% auto;
+  padding: 20px;
+  width: 50%;
+  position: relative;
+`;
+const ModalClose = styled.button`
+  background-color: red;
+  position: absolute;
+  z-index: 1;
+  right: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 1rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+  border: none;
+  &:hover {
+    border: 1px black groove;
+    color: black;
+  }
+`;
 
 const Modal: FC<ModalProps> = ({ children }) => {
   const [shouldShow, setShouldShow] = useState(false);
@@ -16,9 +50,11 @@ const Modal: FC<ModalProps> = ({ children }) => {
       <button onClick={() => setShouldShow(true)}>Open Modal</button>
       {shouldShow && (
         <ModalBackground onClick={() => setShouldShow(false)}>
-          <ModalBody>
-            <button onClick={() => setShouldShow(false)}>close Modal</button>
+          <ModalBody onClick={(e) => e.stopPropagation()}>
             {children}
+            <ModalClose onClick={() => setShouldShow(false)}>
+              <span>x</span>
+            </ModalClose>
           </ModalBody>
         </ModalBackground>
       )}
